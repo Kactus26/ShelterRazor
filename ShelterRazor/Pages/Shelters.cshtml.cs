e3a85645cf30ed1cec60d3c27f2c7695d29a060c
Kactus25 <sasha.baginsky@gmail.com>
2024-02-23 13:33:10 +0300

    Добавьте файлы проекта.

---
+++ ShelterRazor/Pages/Shelters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShelterRazor.Pages
+{
+    public class SheltersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
